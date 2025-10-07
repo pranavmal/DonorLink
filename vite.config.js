0767bc1f00dc61f01a9deada9bea6bfc5f42172e
Cosmoverz <pranav1.malaraju@gmail.com>
2025-10-07 18:42:37 -0500

    Update page linking issues

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        blood: resolve(__dirname, 'blood/index.html'),
+        organ: './src/pages/organ.html',
+        questions: './src/pages/chatbot.html',
+      },
+    },
+  },
 })
