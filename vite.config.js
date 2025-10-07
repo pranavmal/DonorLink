137e5d44c5d31aa47647f7cf4c778b9d28c42f5d
Cosmoverz <pranav1.malaraju@gmail.com>
2025-10-07 18:45:40 -0500

    Fix resolve method call error

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        blood: resolve(__dirname, 'blood/index.html'),
-      },
-    },
-  },
 })
