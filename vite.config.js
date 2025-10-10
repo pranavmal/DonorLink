c897c8187f755c2a002c7b25f548771697a89ef1
Cosmoverz <pranav1.malaraju@gmail.com>
2025-10-10 01:13:24 -0500

    Fix linking issue

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        blood: "blood.html",
+      },
+    },
+  },
 })
